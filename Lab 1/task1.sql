5498910237f650d2dd6273a1257b1145b6920773
Kamran <muhammadkamran5862@gmail.com>
2024-01-19 00:01:15 +0500

    m

--- Lab 1/task1.sql
+++ Lab 1/task1.sql
@@ -1,7 +1,21 @@
 CREATE DATABASE TestDB_2022_CS_53;
-
+GO
+USE TestDB_2022_CS_53;
+GO
 CREATE TABLE student(
-  RollNo int,
+  RollNo int PRIMARY KEY,
   name VARCHAR(50),
-  Section VARCHAR(50)
+  Section VARCHAR(50),
+  Semester INT
 );
+GO
+
+SELECT * FROM student;
+INSERT INTO student
+(RollNo, name, Section, Semester)
+VALUES
+(1,'Kamran','A',4),
+(2,'Hamza','A',4),
+(3,'Muddasar','A',4),
+(4,'Saad','A',4);
+
