ed021959576e58a1420bf1381301e0e6ed4334fc
Kamran <muhammadkamran5862@gmail.com>
2024-01-23 20:50:09 +0500

    m

--- Lab 1/task1.sql
+++ Lab 1/task1.sql
@@ -1,14 +1,11 @@
 CREATE DATABASE TestDB_2022_CS_53;
-GO
 USE TestDB_2022_CS_53;
-GO
 CREATE TABLE student(
   RollNo int PRIMARY KEY,
   name VARCHAR(50),
   Section VARCHAR(50),
   Semester INT
 );
-GO
 
 SELECT * FROM student;
 INSERT INTO student
