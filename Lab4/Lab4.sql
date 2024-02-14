849a534a477ba98ced371f76023316238a3e4d1a
Muhammad Kamran <muhammadkamran5862@gmail.com>
2024-02-15 02:12:58 +0500

    Task completed

--- Lab4/Lab4.sql
+++ Lab4/Lab4.sql
@@ -15,3 +15,28 @@ SELECT Country FROM Suppliers GROUP BY Country HAVING COUNT(Country)>=2;
 
 --Q4--
 SELECT Month(ShippedDate) AS [Month Number], COUNT(OrderID) AS [Order Delyaed] FROM Orders WHERE ShippedDate>RequiredDate Group BY Month(ShippedDate);
+***
+
+
+--Q5--
+SELECT OrderID, SUM(Discount) AS Discount FROM [Order Details] WHERE Discount>0 GROUP BY OrderID;
+***
+
+
+--Q6--
+SELECT ShipCity, COUNT(OrderID) AS [Number of Orders] FROM Orders WHERE ShipCountry='USA' AND YEAR(ShippedDate)='1997' GROUP BY ShipCity;
+***
+
+
+--Q7--
+SELECT ShipCountry AS Country, COUNT(ShippedDate) AS [Orders Delayed] FROM Orders WHERE ShippedDate>RequiredDate GROUP BY ShipCountry;
+***
+
+
+--Q8--
+SELECT OrderID, SUM(Discount) AS Discount, SUM(UnitPrice*Quantity) AS [Total Price] FROM [Order Details] WHERE Discount>0 GROUP BY OrderID;
+***
+
+
+--Q9--
+SELECT ShipRegion, ShipCity, COUNT(OrderID) AS Orders FROM Orders WHERE YEAR(ShippedDate)='1997' AND ShipRegion IS NOT NULL  GROUP BY ShipRegion, ShipCity;
